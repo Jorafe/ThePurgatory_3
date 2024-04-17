401e0d34a7918218b7c2f260b4a836d53e6772de
Jorafe <joanrafe15@gmail.com>
2024-04-17 11:01:01 +0200

    Coin sound

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+     public AudioSource source;
+
+     public AudioClip coinSound;
+
+    private BoxCollider2D boxCollider;
+
+    /*[SerializeField] private float cantidadPuntos;
+
+    [SerializeField] private Puntos puntos;*/
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+      source = GameObject.Find("sfxmanager").GetComponent<AudioSource>();
+    }
+
+ 
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+       
+        if(collider.gameObject.tag == "Player")
+    {
+        /*puntos.SumarPuntos(cantidadPuntos);*/
+       source.PlayOneShot(coinSound);
+       Destroy(gameObject);
+    }
+
+    }
+}

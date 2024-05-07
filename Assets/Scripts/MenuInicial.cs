2c642e5b50b854b3d616c9f1a3cc751877e228fc
Joan <joanrafe15@gmail.com>
2024-05-07 21:10:32 +0200

    Menu inicio

---
+++ Assets/Scripts/MenuInicial.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public Text scoreText;
+
+    public int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadScore();
+       
+    }
+
+    void LoadScore()
+    {
+        scoreText.text = "Points: " + score.ToString();
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LoadInicio()
+    {
+        SceneManager.LoadScene("Inicio");
+    }
+
+    public void LoadMuerte()
+    {
+        SceneManager.LoadScene("Muerte");
+    }
+
+    public void LoadRanking()
+    {
+        SceneManager.LoadScene("Ranking");
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
